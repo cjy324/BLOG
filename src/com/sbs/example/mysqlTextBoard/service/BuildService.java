e13745eda231ff9795df49ac4d7abab5c7363aa3
cjy324 <juy32400@naver.com>
2020-12-12 15:05:43 +0900

    20.12.12 메뉴바 적용 및 게시판 목록 동적 생성 적용(**)

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.sbs.example.mysqlTextBoard.container.Container;
 import com.sbs.example.mysqlTextBoard.dto.Article;
+import com.sbs.example.mysqlTextBoard.dto.Board;
 import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class BuildService {
@@ -19,18 +20,17 @@ public class BuildService {
 
 		Util.rmdir("site"); // 기존 site 폴더 삭제
 		Util.mkdir("site"); // 신규 site 폴더 생성
-		
-		//site_template에 있는 app.css(원본)를 복사해 site폴더 생성시 그 안에 복사본 붙여넣기
+
+		// site_template에 있는 app.css(원본)를 복사해 site폴더 생성시 그 안에 복사본 붙여넣기
 		Util.copy("site_template/app.css", "site/app.css");
 
 		List<Article> articles = articleService.getArticlesForPrint();
 
-		String head = Util.getFileContents("site_template/head.html"); // head.html 가져오기
+		String head = getHeadHtml();
 		String foot = Util.getFileContents("site_template/foot.html"); // foot.html 가져오기
 
-		int articleIndex = 0;
-		
 		System.out.println("= article 상세페이지 생성 =");
+		int articleIndex = 0;
 		for (Article article : articles) {
 			StringBuilder html = new StringBuilder();
 
@@ -53,7 +53,7 @@ public class BuildService {
 				html.append("<a href=\"" + (article.id + 1) + ".html" + "\">다음글</a><br>");
 			}
 			html.append("</div>");
-			
+
 			// head.html과 마찬가지로 foot.html을 따로 생성해 아래쪽에 붙임
 			html.append(foot);
 
@@ -64,9 +64,42 @@ public class BuildService {
 
 			System.out.println(path + " 생성");
 			articleIndex++;
-			
+
 		}
 		System.out.println("= article 상세페이지 생성 종료 =");
+	}
+
+	private String getHeadHtml() {
+		List<Board> boards = articleService.getBoards();
+
+		String head = Util.getFileContents("site_template/head.html"); // head.html 가져오기
+		StringBuilder boardMenuContentHtml = new StringBuilder();
+		
+		for (Board board : boards) {
+			boardMenuContentHtml.append("<li>");
+
+			String link = board.code + "-list-1.html";
+
+			boardMenuContentHtml.append("<a href=\"" + link + "\" class=\"block\">");
+
+			String iClass = "fas fa-clipboard-list"; // defult 아이콘
+
+			if (board.code.contains("notice")) { // 공지사항 게시판 아이콘
+				iClass = "fas fa-exclamation";
+			} else if (board.code.contains("free")) { // 자유 게시판 아이콘
+				iClass = "fas fa-users";
+			}
+
+			boardMenuContentHtml.append("<i class=\"" + iClass + "\"></i>");
+			boardMenuContentHtml.append(" <span>" + board.name + "</span>");
+			boardMenuContentHtml.append("</a>");
+
+			boardMenuContentHtml.append("</li>");
+
+		}
+
+		head = head.replace("[게시판 이름 블록]", boardMenuContentHtml.toString());
+		return head;
 	}
 
 }
