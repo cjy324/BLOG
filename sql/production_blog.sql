4b27d251c0232eefbc80b03144bf9182d32a1c7a
cjy324 <juy32400@naver.com>
2021-01-14 18:15:55 +0900

    21.01.14 DB에 tag 도입

---
+++ sql/production_blog.sql
@@ -0,0 +1,42 @@
+## 사이트 운영 시작 ##
+
+#기존 사이트 게시글 및 사용자 초기화 
+TRUNCATE `article`;
+TRUNCATE `member`;
+TRUNCATE `board`;
+
+# 관리자 생성
+SELECT * FROM `member`;
+INSERT INTO `member`
+SET loginId = 'admin',
+loginPw = 'admin1!',
+`name` = 'Dev_J';
+
+# 공지사항 게시판 생성
+INSERT INTO `board`
+SET `name` = '공지사항',
+`code` = 'notice';
+
+# JAVA 관련 게시판 생성
+INSERT INTO `board`
+SET `name` = 'JAVA',
+`code` = 'java';
+
+# HTML & CSS & JS 관련 게시판 생성
+INSERT INTO `board`
+SET `name` = 'HTML & CSS & JS',
+`code` = 'html';
+
+SELECT * FROM `article`;
+SELECT * FROM `member`;
+SELECT * FROM `board`;
+SELECT * FROM ga4DataPagePath;
+
+
+SELECT COUNT(id)
+FROM `board`;
+
+SELECT COUNT(id)
+FROM `article`;
+
+
---
+++ sql/util/tag_blog.sql
@@ -0,0 +1,60 @@
+USE textBoard;
+
+# 변수 세팅 필요 시작 #
+SET @articleId = 12;
+SET @hashsStr = '#HTML #태그 #엘리먼트';
+# 변수 세팅 필요 끝 #
+
+# 해시태그 구분자
+SET @hashSign = '#';
+
+# 새 해시태그 개수
+SET @hashsCount = LENGTH(@hashsStr) - LENGTH(REPLACE(@hashsStr, @hashSign, ""));
+SET @hashsCount = @hashsCount / LENGTH(@hashSign);
+SET @hashsCount = FLOOR(@hashsCount);
+
+SELECT @hashsCount;
+
+# 삭제되어야 할 것 삭제, v2
+DELETE FROM tag
+WHERE id IN (
+    SELECT OT.id
+    FROM tag AS OT
+    LEFT JOIN (
+        WITH RECURSIVE rgen(n) AS (
+            SELECT 1
+            UNION ALL
+            SELECT n + 1
+            FROM rgen
+            WHERE n < @hashsCount
+        )
+        SELECT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(@hashsStr, @hashSign, n + 1), @hashSign, -1)) AS `body`
+        FROM rgen
+    ) AS NT
+    ON OT.body = NT.body
+    WHERE NT.body IS NULL
+    AND relTypeCode = 'article'
+    AND relId = @articleId
+);
+
+# 추가 되어야 하는 녀석들 추가
+INSERT INTO tag
+(regDate, updateDate, relTypeCode, relId, `body`)
+SELECT NOW(), NOW(), 'article', @articleId, NT.body
+FROM (
+    WITH RECURSIVE rgen(n) AS (
+        SELECT 1
+        UNION ALL
+        SELECT n + 1
+        FROM rgen
+        WHERE n < @hashsCount
+    )
+    SELECT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(@hashsStr, @hashSign, n + 1), @hashSign, -1)) AS `body`
+    FROM rgen
+) AS NT
+LEFT JOIN tag AS OT
+ON OT.relTypeCode = 'article'
+AND OT.relId = @articleId
+AND NT.body = OT.body
+WHERE OT.id IS NULL
+AND NT.body != '';
