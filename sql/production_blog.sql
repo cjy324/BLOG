de35184224d140169f90f152028ca49f81b8f688
cjy324 <juy32400@naver.com>
2021-01-15 16:03:57 +0900

    21.01.15 sql파일 위치 변경

--- sql/production_blog.sql
+++ sql/production_blog.sql
@@ -31,6 +31,7 @@ SELECT * FROM `article`;
 SELECT * FROM `member`;
 SELECT * FROM `board`;
 SELECT * FROM ga4DataPagePath;
+SELECT * FROM tag;
 
 
 SELECT COUNT(id)
@@ -39,4 +40,30 @@ FROM `board`;
 SELECT COUNT(id)
 FROM `article`;
 
+# 전체게시물 + 태그정보 조회
+SELECT A.id,
+A.title,
+IFNULL(GROUP_CONCAT(T.body), '없음') AS tags
+FROM article AS A
+LEFT JOIN tag AS T
+ON A.id = T.relId
+AND T.relTypeCode = 'article'
+GROUP BY A.id
 
+#GROUP_CONCAT - 구분자를 활용한 문자열 묶음
+#GROUP BY로 된 레코드들 중에서 구분자를 이용하여 해당하는 문자열을 보고자 할때 사용
+#기본적으로 GROUP BY를 하게되면 여러개의 레코드가 합쳐져도 문자열은 하나만 보이게 된다.
+#GROUP_CONCAT(컬럼명)을 하면 각 레코드안에 있는 모든 문자열들이 뒤따라 보이게 된다. 
+
+#IFNULL(칼럼명, '원하는 대답')
+#만약 해당 칼럼명안의 데이터가 null이면 '원하는 대답'이 나타나게 한다.
+
+# 게시물 + 태그정보 조회
+SELECT
+IFNULL(GROUP_CONCAT(T.body), '없음') AS tags
+FROM article AS A
+LEFT JOIN tag AS T
+ON A.id = T.relId
+AND T.relTypeCode = 'article'
+WHERE A.id = 12
+GROUP BY A.id
